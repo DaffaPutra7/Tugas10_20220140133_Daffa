680de25e7c99fa58afa7a598411e195615506bdf
DaffaPutra7 <putradika3604@gmail.com>
2024-06-22 02:07:27 +0700

    first commit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    base: "/Tugas10_20220140138_Daffa/",
+    plugins: [react()],
+})
