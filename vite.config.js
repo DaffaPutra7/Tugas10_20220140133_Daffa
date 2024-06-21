8b5fa2c4926521c58a197f38b6902af9f57692e4
DaffaPutra7 <putradika3604@gmail.com>
2024-06-22 02:08:17 +0700

    p

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/Tugas10_20220140138_Daffa/",
+    base: "/Tugas10_20220140133_Daffa/",
     plugins: [react()],
 })
